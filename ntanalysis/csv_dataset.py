6302e49efa6fe77975e56aba13c0d4c6e7659493
EgorovGA <egorov.ga@minc.tech>
2024-03-03 18:53:24 +0300

    speedup dataset construction

--- ntanalysis/csv_dataset.py
+++ ntanalysis/csv_dataset.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import torch
-from tqdm import tqdm
 
 
 class CsvDataset(torch.utils.data.Dataset):
@@ -15,30 +14,33 @@ class CsvDataset(torch.utils.data.Dataset):
         max_length=None,
         transform=None,
     ):
+        print("Reading dataset from csv...")
         raw_df = pd.read_csv(csv_path)
-        if max_length is None:
-            max_length = 1
-        dim_in = input_size * (10 + 7)
-        dim_out = prediction_size * 7  # * (10 + 7)
+        # main variables TODO delete some
         N = int(len(raw_df) * max_length)
-        # N>=length+input_size*input_gap; N>=length+prediction_size+prediction_distance
-        length = (
-            N - max(input_size * input_gap, prediction_size + prediction_distance) - 1
-        )
-        self.data_in = np.zeros(shape=(length, dim_in), dtype=np.float32)
-        self.data_out = np.zeros(shape=(length, dim_out), dtype=np.float32)
-        print("Loading dataset...")
-        for x in tqdm(range(length)):
-            for i in range(input_size):
-                row_source = x + i * input_gap
-                y_bias = (10 + 7) * i
-                for y in range(0, 10):
-                    self.data_in[x][y_bias + y] = raw_df.iloc[row_source, 1 + y]
-                self.data_in[x][y_bias + 10 + raw_df["TIME_dow"][row_source]] = 1
-            for i in range(prediction_size):
-                row_source = x + prediction_distance + i
-                y_bias = 7 * i
-                self.data_out[x][y_bias + raw_df["TIME_dow"][row_source]] = 1
+        raw_df = raw_df.iloc[:N, :]
+        # convert day of weeks to one-hot
+        one_hot = pd.get_dummies(raw_df["TIME_dow"])
+        raw_df = raw_df.iloc[:, 1:11].join(one_hot)
+        raw_np = raw_df.to_numpy().astype(np.float32)
+        # stack inputs
+        inputs = []
+        for i in range(input_size):
+            start = i * input_gap
+            rest = (input_size - 1) * input_gap - start + 1
+            inputs.append(raw_np[start:-rest, :])
+        self.data_in = np.concatenate(inputs, axis=1)
+        # stack outputs
+        outputs = []
+        for i in range(prediction_size):
+            start = i
+            rest = prediction_size - start
+            outputs.append(raw_np[prediction_distance + start : -rest, -7:])
+        self.data_out = np.concatenate(outputs, axis=1)
+        # trim to match sizes
+        length = min(self.data_out.shape[0], self.data_in.shape[0])
+        self.data_in = self.data_in[:length, :]
+        self.data_out = self.data_out[:length, :]
 
     def __len__(self):
         return self.data_in.shape[0]
