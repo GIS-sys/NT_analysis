76740f501b9e5ad34cb5878b209a80dc95b45364
EgorovGA <egorov.ga@minc.tech>
2024-03-04 21:41:42 +0300

    add proper annotations

--- ntanalysis/showcase.py
+++ ntanalysis/showcase.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import hydra
 import lightning.pytorch as pl
@@ -14,7 +14,6 @@ from ntanalysis.utils import get_default_trainer
 from omegaconf import DictConfig
 
 
-BAD_POINT_THRESHOLD = 0.9
 BAD_POINT_HEIGHT = 2
 Y_AXIS = [0, 1.1]
 TOTAL_FRAMES = 720
@@ -22,15 +21,27 @@ FPS = 24
 TRAILING_PLOT = 0.1
 TRAILING_CUMMEAN = 1000
 ZIGZAG_THRESHOLDS = [(1, 0.4), (1, 0.8), (-1, 0.32)]
+ZIGZAG_COLORS = ["black", "orange", "red"]
+ZIGZAG_LABELS = [
+    lambda _: "",
+    lambda d: ("ВНИМАНИЕ\n" + d.strftime("%Y/%m/%d, %H:%M:%S")),
+    lambda d: (
+        "ТРЕВОГА\n"
+        + d.strftime("%Y/%m/%d, %H:%M:%S\n")
+        + (d + timedelta(days=4)).strftime("%Y/%m/%d, %H:%M:%S")
+    ),
+]
+FONT_SIZE = 20
 ARROW_PROPS = {"width": 1}
+WIDTH_BAD_POINT = 10
 
 
-def animate_plot(t, data_plot, zigzag_base_data):
+def animate_plot(t, pred_data, bad_points):
     # Main vars
     SLIDER_LENGTH = t.shape[0]
     # Calculate thresholds
     zigzag_index = 0
-    arr = zigzag_base_data
+    arr = pred_data
     zigzag_positions = [0]
     while True:
         sign, thresh = ZIGZAG_THRESHOLDS[zigzag_index]
@@ -46,12 +57,25 @@ def animate_plot(t, data_plot, zigzag_base_data):
     fig, ax = plt.subplots()
     plt.subplots_adjust(bottom=0.25)
     # Initial plot
-    for label, datum in data_plot:
-        ax.plot(t, datum, label=label)
+    ax.plot(t, pred_data, label="Предсказание")
+    ax.plot(t, bad_points, label="Проблемы", linewidth=WIDTH_BAD_POINT)
     for i, pos in enumerate(zigzag_positions):
-        condition = ZIGZAG_THRESHOLDS[i % len(ZIGZAG_THRESHOLDS)]
+        zigzag_i = i % len(ZIGZAG_THRESHOLDS)
+        label = ZIGZAG_LABELS[zigzag_i](t[pos])
         ax.annotate(
-            str(condition), (t[pos], zigzag_base_data[pos]), arrowprops=ARROW_PROPS
+            label,
+            (t[pos], pred_data[pos]),
+            arrowprops=ARROW_PROPS,
+            color=ZIGZAG_COLORS[zigzag_i],
+            fontsize=FONT_SIZE,
+        )
+    for x in np.where(bad_points > 0.5)[0]:
+        ax.annotate(
+            t[x].strftime("Запись в журнале\n%Y/%m/%d, %H:%M:%S"),
+            (t[x], pred_data[x]),
+            arrowprops=ARROW_PROPS,
+            color="black",
+            fontsize=FONT_SIZE,
         )
     ax.set_xlim(t[0], t[1])
     ax.set_ylim(Y_AXIS)
@@ -104,7 +128,6 @@ def showcase(cfg: DictConfig):
 
     answers = trainer.predict(model, datamodule=dm)
     answers = np.concatenate(answers)
-    print(answers)
 
     # TODO FIX TIME AXIS INTERVALS IF NEEDED
     # t = np.linspace(0, 1, answers.shape[0])
@@ -113,17 +136,11 @@ def showcase(cfg: DictConfig):
         datetime.fromtimestamp(1664613100),
         answers.shape[0],
     )
-    data_plot = []
     input_end = cfg.model.input_size * 10
     output_end = input_end + cfg.model.prediction_size * 1
-    data_plot.append(("output", answers[:, input_end]))
-    data_plot.append(
-        (
-            "bad point",
-            (answers[:, input_end] > BAD_POINT_THRESHOLD).astype(int) * BAD_POINT_HEIGHT,
-        )
-    )
-    # data_plot.append(("prediction", answers[:, output_end]))
+    # data_plot.append(("output", answers[:, input_end]))
+    bad_points = np.where(answers[:-1, input_end] - answers[1:, input_end] >= 0.2, 1, 0)
+    bad_points = np.insert(bad_points, 0, np.zeros(1))
     pred_cumsum = np.cumsum(answers[:, output_end])
     pred_cummean_trailing = (
         pred_cumsum[TRAILING_CUMMEAN:] - pred_cumsum[:-TRAILING_CUMMEAN]
@@ -131,8 +148,7 @@ def showcase(cfg: DictConfig):
     pred_plot = np.concatenate(
         (answers[:TRAILING_CUMMEAN, output_end], pred_cummean_trailing)
     )
-    data_plot.append(("prediction", pred_plot))
-    animate_plot(t, data_plot, pred_plot)
+    animate_plot(t, pred_plot, bad_points)
 
 
 if __name__ == "__main__":
